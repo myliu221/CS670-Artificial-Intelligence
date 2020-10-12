dcfc7dd21fc91b4565e8e49baddb434298da6d66
Mingyu Liu <myliu221@bu.edu>
2020-10-12 18:32:20 -0400

    Assignment2_a_star

--- Assignments/Assignment2/Assignment2_Mingyu Liu.py
+++ Assignments/Assignment2/Assignment2_Mingyu Liu.py
@@ -21,6 +21,14 @@ example_2_goal = np.array([[ 1,  2,  3,  4],
                            [ 9, 10, 11, 12],
                            [13, 14, 15,  0]])
 
+example_3_goal = np.array([[1, 2, 3],
+                           [4, 5, 6],
+                           [7, 8, 0]])
+
+example_3_start = np.array([[1, 0, 5],
+                            [3, 7, 2],
+                            [4, 8, 6]])
+
 # For a given current state, move, and goal, compute the new state and its h'-score and return them as a pair. 
 def make_node(state, row_from, col_from, row_to, col_to, goal):
     # Create the new state that results from playing the current move. 
@@ -68,48 +76,49 @@ def a_star(start, goal, expand):
         for j in range(width):
             if start[i ,j] > 0 and start[i, j] != goal[i, j]:
                 mismatch_count += 1
-    #save start state and its h'-score into good_state_pair
-    good_state_pair = (start, mismatch_count)
-    #save start state array into good_state
-    good_state = [good_state_pair[0]]
+    #save start state and its h'-score into open_list
+    open_list = [([start],mismatch_count)]
     #compare start state and goal
-    compare_array = np.array_equal(good_state_pair[0], goal)
+    compare_array = np.array_equal(open_list[0][0][-1], goal)
     #check whether reach the goal state
     while not compare_array:
         #create all states that can be reached from the current state and return state and h'-score
-        node_list = slide_expand(good_state_pair[0], goal) 
+        node_list = expand(open_list[0][0][-1], goal) 
         #check new states and delete same states as ancestors to avoid a search cycle
         node_list_del = []
         for i in range(len(node_list)):
-            for j in range(len(good_state)):
-                if np.array_equal(node_list[i][0],good_state[j]):
+            for j in range(len(open_list[0][0])):
+                if np.array_equal(node_list[i][0],open_list[0][0][j]):
                     node_list_del.append(i)
-        for i in node_list_del:
+        for i in sorted(node_list_del,reverse=True):
             del node_list[i] 
-        # if node_list is empty, which means all the new states are same as ancestors, then stop searching and return empty list
+        # if node_list is empty, which means all the new states are same as ancestors, then delete first element of open_list
         if len(node_list) == 0:
+            del open_list[0]
+        # if node_list is not empty, add all the children states and their f'-scores(current_depth + h'-score) in open_list
+        else:
+            for i in range(len(node_list)):
+                # copy open_list 
+                ancestor = open_list[0][0].copy()
+                ancestor.append(node_list[i][0])
+                #save each new state and its ancestors into a list, and calculate its f'-score
+                node_list_sub=(ancestor,node_list[i][1]+len(open_list[0][0]))
+                open_list.append(node_list_sub)
+            # remove the expanded node
+            del open_list[0]
+            #sort f'-score of the list of open_list
+            for i in range(0, len(open_list)):           
+                for j in range(0, len(open_list)-i-1):  
+                    if (open_list[j][1] > open_list[j + 1][1]):  
+                        a = open_list[j]  
+                        open_list[j]= open_list[j + 1]  
+                        open_list[j + 1]= a 
+        #if open_list is empty, which means that the problem does not have a solution, return empty list
+        if len(open_list) == 0:
             return []
-        # good_state_pair = node_list[0]
-        # for i in range(len(node_list)): 
-        #     if node_list[i][1] < good_state_pair[1]: 
-        #         good_state_pair = node_list[i]
-        #     elif node_list[i][1] == good_state_pair[1]:
-        #         node_list1 = slide_expand(node_list[i][0], goal) 
-        #         node_list2 = slide_expand(good_state_pair[0], goal)         
-        
-        #sort h'-score of the list of open nodes
-        for i in range(0, len(node_list)):           
-            for j in range(0, len(node_list)-i-1):  
-                if (node_list[j][1] > node_list[j + 1][1]):  
-                    a = node_list[j]  
-                    node_list[j]= node_list[j + 1]  
-                    node_list[j + 1]= a         
-        # choose the smallest h'-score for next round
-        good_state_pair = node_list[0]
-        compare_array = np.array_equal(good_state_pair[0], goal)
-        # add the current good state into good_state
-        good_state.append(good_state_pair[0])
-    return good_state
+        # choose the smallest f'-score for next round
+        compare_array = np.array_equal(open_list[0][0][-1], goal)
+    return open_list[0][0]
 
 # Find and print a solution for a given slide puzzle, i.e., the states we need to go through 
 # in order to get from the start state to the goal state.
@@ -139,3 +148,76 @@ def slide_solver(start, goal):
             print(space + 'V')
 
 slide_solver(example_1_start, example_1_goal)       # Find solution to example_1
+
+
+
+
+# For a given current state, move, and goal, compute the new state and its h'-score and return them as a pair. 
+def make_node_improved(state, row_from, col_from, row_to, col_to, goal):
+    # Create the new state that results from playing the current move. 
+    (height, width) = state.shape
+    new_state = np.copy(state)
+    new_state[row_to, col_to] = new_state[row_from, col_from]
+    new_state[row_from, col_from] = 0
+    
+    # Count the mismatched numbers and use this value as the h'-score (estimated number of moves needed to reach the goal).
+    mismatch_count = 0
+    for i in range(height):
+        for j in range(width):
+            if new_state[i ,j] > 0 and new_state[i, j] != goal[i, j]:
+                mismatch_count += 1
+   
+    return (new_state, mismatch_count)
+
+# For given current state and goal state, create all states that can be reached from the current state
+# (i.e., expand the current node in the search tree) and return a list that contains a pair (state, h'-score)
+# for each of these states.   
+def slide_expand_improved(state, goal):
+    node_list = []
+    (height, width) = state.shape
+    (empty_row, empty_col) = np.argwhere(state == 0)[0]     # Find the position of the empty tile
+    
+    # Based on the positin of the empty tile, find all possible moves and add a pair (new_state, h'-score)
+    # for each of them.
+    if empty_row > 0:
+        node_list.append(make_node_improved(state, empty_row - 1, empty_col, empty_row, empty_col, goal))
+    if empty_row < height - 1:
+        node_list.append(make_node_improved(state, empty_row + 1, empty_col, empty_row, empty_col, goal))
+    if empty_col > 0:
+        node_list.append(make_node_improved(state, empty_row, empty_col - 1, empty_row, empty_col, goal))
+    if empty_col < width - 1:
+        node_list.append(make_node_improved(state, empty_row, empty_col + 1, empty_row, empty_col, goal))
+    
+    return node_list
+
+
+# Find and print a solution for a given slide puzzle, i.e., the states we need to go through 
+# in order to get from the start state to the goal state.
+def slide_solver_improved(start, goal):
+    solution = a_star(start, goal, slide_expand_improved)
+    if not solution:
+        print('This puzzle has no solution. Please stop trying to fool me.')
+        return
+        
+    (height, width) = start.shape
+    if height * width >= 10:            # If numbers can have two digits, more space is needed for printing
+        digits = 2
+    else:
+        digits = 1
+    horizLine = ('+' + '-' * (digits + 2)) * width + '+'
+    for step in range(len(solution)):
+        state = solution[step]
+        for row in range(height):
+            print(horizLine)
+            for col in range(width):
+                print('| %*d'%(digits, state[row, col]), end=' ')
+            print('|')
+        print(horizLine)
+        if step < len(solution) - 1:
+            space = ' ' * (width * (digits + 3) // 2)
+            print(space + '|')
+            print(space + 'V')
+
+
+
+
